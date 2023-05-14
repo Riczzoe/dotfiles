2bfb6cc924f416b37d705b44f6384945cbee68f2
Rice <86246881+JohnFrankz@users.noreply.github.com>
2023-05-14 01:33:47 +0000

    Add dev tools download script

---
+++ dev.sh
@@ -0,0 +1,15 @@
+sudo apt update
+# download gcc, g++, make...
+# sudo apt install build-essential
+
+# download valgrind
+# wget https://sourceware.org/pub/valgrind/valgrind-3.21.0.tar.bz2
+# tar xvf valgrind-3.21.0.tar.bz2
+# cd valgrind-3.21.0
+# ./configure
+# make
+# sudo make install
+sudo apt install valgrind
+
+# download gdb
+sudo apt install gdb
